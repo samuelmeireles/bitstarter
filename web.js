26daa53fb353e15715bef3826f5c472723a5550f
Samuel Meireles <samuelmeireles.ds@gmail.com>
2013-07-11 04:05:32 +0000

    Add index.html to hello msg

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  hello = fs.readFileSync('index.html', encoding='utf-8');
+  response.send(hello);
 });
 
 var port = process.env.PORT || 5000;
